13f9efa4f136091d468e8654b297954228629f9e
Chonglin Guan <gg575abc1204@gmail.com>
2023-06-20 11:35:53 -0700

    Second Commit

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,2 +1,39 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class StringCalculatorTest {
+
+    private StringCalculator calculatedString;
+
+    @BeforeEach
+    public void setUp() {
+        calculatedString = new StringCalculator();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        calculatedString = null;
+    }
+
+    @Test
+    public void test1() {
+        int value = calculatedString.add("1, 500");
+        assertEquals(501, value);
+    }
+
+    @Test
+    public void test2() {
+        int value = calculatedString.add("1, 1000");
+        assertEquals(501, value);
+    }
+
+    @Test
+    public void test3() {
+        int value = calculatedString.add("1, -1");
+        assertEquals(0, value);
+    }
 }
